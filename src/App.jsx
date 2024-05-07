fbe2959993b1d7d234ef8c52573a6b123c46bf43
Miss-Sonia <soniaihemeo@gmail.com>
2024-05-07 21:52:59 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+import HeroSection from './components/herosection'
+
+function App() {
+
+  return (
+    <HeroSection/>
+  )
+}
+
+export default App
---
+++ src/components/herosection.jsx
@@ -0,0 +1,12 @@
+import react from 'react';
+
+const HeroSection = () => {
+    return (
+        <div>
+          <h1>Hello!</h1>
+        </div>
+      );
+    
+}
+
+export default HeroSection;
