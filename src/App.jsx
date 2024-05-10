c7f42f6d5037d6ba8e0e0ebca979a89696422345
anayoosuji <simplyarnie1@gmail.com>
2024-05-10 14:24:05 +0100

    2nd Commit...Social Icons

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
 import HeroSection from './components/Herosection/herosection'
+import {FaFacebook, FaInstagram, FaYoutube} from 'react-icons/fa'
 
 function App() {
 
--- src/components/Herosection/herosection.jsx
+++ src/components/Herosection/herosection.jsx
@@ -7,9 +7,9 @@ const HeroSection = () => {
         <div className='bundle'>
             <div className="right-col">
                 <h1>NaijaFlip: Your <span className='txt'>Fun escape </span> <div>to Nigerian Culture!</div></h1>
-                <a href="./Signin"><button className='header_button'>Get Started</button></a>
+                <a href="../Signup"><button className='header_button'>Get Started</button></a>
                 <a href="./Login">
-                <button className='login'>Login</button></a>
+                <button className='login'>Sign in</button></a>
             </div>
             <div className="left-col">
                 <img src={masq3} />
