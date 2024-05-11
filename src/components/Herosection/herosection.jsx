5004305e0430e1602361861761e9ae3f9aa2285a
anayoosuji <simplyarnie1@gmail.com>
2024-05-11 19:51:43 +0100

    7t commit

--- src/components/Herosection/herosection.jsx
+++ src/components/Herosection/herosection.jsx
@@ -9,7 +9,7 @@ const HeroSection = () => {
             <div className="right-col">
                 <h1>NaijaFlip: Your <span className='txt'>Fun escape </span> <div>to Nigerian Culture!</div></h1>
                 <button className='header_button '><Link className= "my-links" to="/Signup">Get Started</Link></button>
-                <button className='login'><Link className= "my-links"to="/Login">Sign in</Link></button>
+                <button className='login'><Link className= "my-link"to="/Login">Sign in</Link></button>
             </div>
             <div className="left-col">
                 <img src={msq5} />
