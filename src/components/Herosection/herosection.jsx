1ab9eb061fc36a1b5e231e879d097798850de665
anayoosuji <simplyarnie1@gmail.com>
2024-05-11 08:14:18 +0100

    3rd Commit...Optimised HERO Image & Button Hover effect

--- src/components/Herosection/herosection.jsx
+++ src/components/Herosection/herosection.jsx
@@ -1,5 +1,5 @@
 import react from 'react';
-import masq3 from '../../assets/masq3.png'
+import msq5 from '../../assets/msq5.png'
 import './herosection.css'
 
 const HeroSection = () => {
@@ -7,12 +7,12 @@ const HeroSection = () => {
         <div className='bundle'>
             <div className="right-col">
                 <h1>NaijaFlip: Your <span className='txt'>Fun escape </span> <div>to Nigerian Culture!</div></h1>
-                <a href="../Signup"><button className='header_button'>Get Started</button></a>
+                <a href="../Signup"><button className='header_button '>Get Started</button></a>
                 <a href="./Login">
                 <button className='login'>Sign in</button></a>
             </div>
             <div className="left-col">
-                <img src={masq3} />
+                <img src={msq5} />
             </div>
         </div>
 
