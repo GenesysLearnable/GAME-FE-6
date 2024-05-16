d6a066b0963088f74825c8d456bce39828fb2cd6
anayoosuji <simplyarnie1@gmail.com>
2024-05-16 14:01:54 +0100

    Resolving NoPage

--- src/components/Header/Header2/GamePageHeader.jsx
+++ src/components/Header/Header2/GamePageHeader.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import styles from './GamePageHeader.module.css'
+
 
 export default function GamePageHeader() {
   return (
-    <div>GamePageHeader</div>
+      <header className={styles.gamePage_nav}>
+          <div className={styles.gamePage_navbar}>
+                <div className={styles.brandLogo}>
+                    <img src= 'images/brandLogo.png' alt='Hero pic' />
+                </div>
+                <div className={styles.gamePage_navlinks}>
+                      <div>Home</div>
+                      <div>Tutorial</div>
+                      <div>Cards</div>
+                      <div>Game History</div>
+                      <div>LeaderBoard</div>
+                      <div>Settings</div>
+                      <img src= 'images/2.png' alt='Hero pic' />
+                     
+                </div>
+          </div>
+  
+      </header>
   )
 }
