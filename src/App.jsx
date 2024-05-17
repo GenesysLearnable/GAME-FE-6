22ba071a31e7624c048c6da031840e2d667a79a7
KamsiKeren <156103167+KamsiKeren@users.noreply.github.com>
2024-05-17 17:43:15 +0100

    Reinstalling Card Game Template

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignIn from "./components/SignIn/SignIn";
+import GamePage from "./pages/GamePage/GamePage";
+// import NoPage from './pages/NoPage';
+import SignUp from "./components/SignUp/SignUp";
+import Home from "./pages/Home/Home";
+import "./App.css";
+import NoPage from "./pages/NoPage";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/SignIn" element={<SignIn />} />
+        <Route path="/SignUp" element={<SignUp />} />
+        <Route path="/GamePage" element={<GamePage />} />
+        <Route path="*" element={<NoPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/Nopage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NoPage() {
+  return (
+    <div>
+        <h1>Error: Kindly go to a valid page. Thanks</h1>
+    </div>
+  )
+}
+
+export default NoPage;
